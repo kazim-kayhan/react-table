a88eb9f8000f5f0f9b3c1e0d330b46fa4f6776cc
Kazim Kayhan <mail4kazim.kayhan@gmail.com>
2023-05-19 22:40:55 +0430

    feat: add a basic react table

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import { BasicReactTable } from './components/BasicReactTable'
+import { BasicTable } from './components/BasicTable'
+
+function App() {
+  return (
+    <div className='App'>
+      {/* <BasicTable /> */}
+      <BasicReactTable/>
+    </div>
+  )
+}
+
+export default App
